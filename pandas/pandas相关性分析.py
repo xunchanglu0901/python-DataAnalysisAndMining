4135636371e24f9f167c7b1cac4e63a05111c716
xunchanglu0901 <41165002+xunchanglu0901@users.noreply.github.com>
2018-09-13 14:49:43 +0800

    upload

--- pandas/pandas相关性分析.py
+++ pandas/pandas相关性分析.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 # 时间    : 2018/9/13 13:56
 # 作者    : xcl
+# 餐饮数据相关性分析
+import pandas as pd
+catering_sale='C:\\Users\\Administrator\\Desktop\\catering_sale_all.xls'
 
+data=pd.read_excel(catering_sale,index_col='日期')
+a=data.corr()
+b=data.corr()['百合酱蒸凤爪']
+c=data['百合酱蒸凤爪'].corr(data['翡翠蒸香茜饺'])
+print(a.unstack(),b,c)
