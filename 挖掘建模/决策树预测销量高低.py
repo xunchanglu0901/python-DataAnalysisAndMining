1af6000e74abdffedc5953e1b3ea26842b7fdec1
xunchanglu0901 <41165002+xunchanglu0901@users.noreply.github.com>
2018-09-18 23:18:18 +0800

    upload

---
+++ 挖掘建模/决策树预测销量高低.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 时间    : 2018/9/18 22:34
+# 作者    : xcl
+
+import pandas as pd
+filename = 'C:/Users/Administrator/Desktop/sales_data.xls'
+
+data=pd.read_excel(filename, index_col='序号')
+data[data=='好']=1
+data[data=='高']=1
+data[data=='是']=1
+data[data != 1]= -1
+
+x=data.iloc[:,:3].values.astype(int)
+y=data.iloc[:,3].values.astype(int)
+from sklearn.tree import DecisionTreeClassifier as DTC
+dtc= DTC(criterion='entropy')
+
+dtc.fit(x,y)
+
+#可视化
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO
+with open("Dtree.dot","w") as f:
+    f = export_graphviz(dtc, out_file=f)
